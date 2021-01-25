13e17fe0c1d6fafcb86ab783773e93b10de9d39e
helene <helene.peignard@gmail.com>
2021-01-25 16:46:32 +0100

    feature 1 : browse page

--- client/portfolio.js
+++ client/portfolio.js
@@ -121,3 +121,13 @@ document.addEventListener('DOMContentLoaded', () =>
     .then(setCurrentProducts)
     .then(() => render(currentProducts, currentPagination))
 );
+
+/**
+ * Browse page
+ * @type {[type]}
+ */
+selectPage.addEventListener('change',event => {
+  fetchProducts(parseInt(event.target.value),selectShow.value)
+    .then(setCurrentProducts)
+    .then(() => render(currentProducts,currentPagination));
+});
