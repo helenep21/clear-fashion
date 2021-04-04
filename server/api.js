bde9356f1506f8a310d2230e0e942ff61002a717
helenep21 <helene.peignard@gmail.com>
2021-04-04 21:26:24 +0200

    new api version

--- server/api.js
+++ server/api.js
@@ -10,10 +10,35 @@ module.exports = app;
 app.use(require('body-parser').json());
 app.use(cors());
 app.use(helmet());
+
 app.options('*', cors());
 
+app.get('/', (request, response) => {
+  response.send({'ack': true});
+});
 
-app.get('/products/search', async(req, response) => {
+
+app.get('/products/search', async (request, response)=> {
+  
+  console.log(request.query);
+  
+  let limit = parseInt(request.query.limit);
+  let brand = request.query.brand;
+  let price = parseInt(request.query.price);
+  console.log(limit);
+  console.log(brand);
+  console.log(price);
+  let res = await db.filteredproducts(limit, brand, price);
+
+  response.send({
+    'limit' : limit,
+    'results' : res
+  });
+})
+
+
+
+app.get('/search', async(req, response) => {
   try{
     let res;
     let meta;
@@ -41,6 +66,15 @@ app.get('/products/search', async(req, response) => {
   }
 })
 
+
+
+
+app.get('/products/:id', async (request, response)=> {
+  response.send( await db.findById(request.params.id))
+})
+
+
+
 app.get('', async (req, response) => {
   console.log("was requested pagination");
   let res = await db.findPage(parseInt(req.query.page),parseInt(req.query.size))
@@ -57,8 +91,6 @@ app.get('', async (req, response) => {
   response.send(products);
   
 });
-
-
 
 app.listen(PORT);
 console.log(`📡 Running on port ${PORT}`);
