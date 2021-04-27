36b1c2293cbe7100b1b4a86914835bae3a97e710
helenep21 <helene.peignard@gmail.com>
2021-04-28 00:06:59 +0200

    wrong link again

--- client/portfolio.js
+++ client/portfolio.js
@@ -71,7 +71,7 @@ const setCurrentProducts = ({result, meta}) => {
 const fetchProducts = async (page = 1, size = 12) => {
   try {
     const response = await fetch(
-      `https://server-fashion.vercel.app//search?page=${page}&size=${size}`
+      `https://server-fashion.vercel.app/search?page=${page}&size=${size}`
     );
     const body = await response.json();
 
